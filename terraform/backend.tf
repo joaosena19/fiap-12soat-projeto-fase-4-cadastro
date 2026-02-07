ab3a34b4b0ae4be8cb2f33a72a494ef02d1ed722
Sena <joaosenadainese@gmail.com>
2026-02-07 11:41:16 -0300

    terraform para banco postgres

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "fiap-12soat-fase3-joao-dainese"
+    key     = "fase4/cadastro-database/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
